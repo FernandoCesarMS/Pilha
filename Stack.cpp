3baabbe195f3e37dae15516d728cd43c08300f07
Fernando Cesar <cmnando.silva@gmail.com>
2020-10-31 17:15:28 -0300

    Arrumando erros no pop

--- Stack.cpp
+++ Stack.cpp
@@ -1,38 +1,46 @@
 #include "Stack.h"
 
-struct Node {
+struct Node{
   unsigned elem;
-  Node* next;
+  Node *next;
 };
 
-Stack::Stack() {
+Stack::Stack(){
   _top = new Node();
   _count = 0;
 }
 
-void Stack::push(unsigned elem) {
+void Stack::push(unsigned elem){
   if (_count == 0){
     _top->elem = elem;
     _count++;
   }
   else{
-    Node* aux;
+    Node *aux;
     aux->elem = _top->elem;
     aux->next = _top->next;
     _top->next = aux;
     _top->elem = elem;
+    _count++;
   }
 }
 
-void Stack::pop() {
- _top->next->elem = _top->elem;
- _top->next->next = _top->next;
+void Stack::pop(){
+  if (_count == 0)
+    throw EmptyException();
+  else{
+    _top->next->elem = _top->elem;
+    _top->next->next = _top->next;
+    _count--;
+  }
 }
 
-unsigned Stack::top() const {
-  return _top->elem;  
+unsigned Stack::top() const{
+  if (_count == 0)
+    throw EmptyException();
+  return _top->elem;
 }
 
-unsigned Stack::count() const {
-  return count; 
+unsigned Stack::count() const{
+  return _count;
 }
